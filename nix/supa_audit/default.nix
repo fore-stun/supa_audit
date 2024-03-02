daed0e3a838d5705d73a3baf5d59978567d5e3e2
David Baynard <git@baynard.dev>
2024-03-02 00:31:44 +0000

    Ignore irrelevant files in nix derivation src

--- nix/supa_audit/default.nix
+++ nix/supa_audit/default.nix
@@ -1,11 +1,14 @@
-{ stdenv, postgresql }:
+{ lib, stdenv, postgresql }:
 
 stdenv.mkDerivation {
   name = "supa_audit";
 
   buildInputs = [ postgresql ];
 
-  src = ../../.;
+  src = builtins.path {
+    name = "supa_audit";
+    path = lib.cleanSource ../../.;
+  };
 
   installPhase = ''
     install -D -t $out/share/postgresql/extension *.sql
